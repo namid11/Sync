4ecbe638a1b6d86e6c017ee768f8a0a7cec3306d
Cyan_on_Mac <namid224y@gmail.com>
2019-11-20 23:13:53 +0900

    感度修正

--- Sync/app/src/main/java/com/example/sync/MainActivity.kt
+++ Sync/app/src/main/java/com/example/sync/MainActivity.kt
@@ -102,10 +102,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener, ShakeDetector.Lis
     // シェイク検知
     override fun hearShake() {
         // やりたい処理を書く
-        Log.d("[LOG] - DEBUG", "get shake !")
-        val jsonObj = JSONObject()
-        jsonObj.put("key", "shake")
-//        runSocket(jsonObj)
+        touchPointManager.shake()
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
@@ -247,8 +244,8 @@ class MainActivity : AppCompatActivity(), SensorEventListener, ShakeDetector.Lis
             postJsonObj.put("key", "scroll")
 
             postJsonObj.put("context", JSONObject().apply {
-                put("x", ((point.x - firstTouchPoint!!.x) / 10).toInt())
-                put("y", ((point.y - firstTouchPoint!!.y) / 10).toInt())
+                put("x", ((point.x - firstTouchPoint!!.x)).toInt())
+                put("y", ((point.y - firstTouchPoint!!.y)).toInt())
             })
 
             runSocket(ipPortManager.getIP(), ipPortManager.getPort(), postJsonObj)
@@ -260,6 +257,12 @@ class MainActivity : AppCompatActivity(), SensorEventListener, ShakeDetector.Lis
             if (time - firstTouchTime < 150) {
                 runSocket(ipPortManager.getIP(), ipPortManager.getPort(), postJsonObj)
             }
+        }
+
+        fun shake() {
+            val jsonObj = JSONObject()
+            jsonObj.put("key", "shake")
+            runSocket(ipPortManager.getIP(), ipPortManager.getPort(), jsonObj)
         }
     }
 }
