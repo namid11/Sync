a855024c0c5d73be95789b5a8fbb2060a6ce9e19
Cyan_on_Win <namid224y@gmail.com>
2019-11-25 21:29:36 +0900

    Winプロジェクト追加

---
+++ Sync Win/My_ShareMouse_alpha/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace My_ShareMouse_alpha {
+    static class Program {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        /// 
+
+        static Form1 form = null;
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            // Formを表示させないための処理
+            if(form == null) {
+                form = new Form1();   // Formのインスタンスは作っておくらしい
+                Application.Run();          // Formインスタンスを渡さずにRun
+            }
+        }
+
+        static public void display() {
+            form.Show();
+        }
+    }
+}
